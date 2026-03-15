0f2c9a191e0ca2c5abf42fab3a0d1318bdcfb464
Raian Rashid <rimonraian2@gmail.com>
2026-03-16 01:21:39 +0600

    tools updated

--- Agent/tools.py
+++ Agent/tools.py
@@ -1,4 +1,4 @@
-from langchain_community.tools import WikipediaQueryRun, DuckDuckGoSearchRun
+from langchain_community.tools import WikipediaQueryRun, DuckDuckGoSearchRun, ArxivQueryRun
 from langchain_community.utilities import WikipediaAPIWrapper
 from langchain_core.tools import Tool
 from datetime import datetime
@@ -6,9 +6,11 @@ import os
 
 def save_to_txt(data: str, filename: str = "research_output.txt"):
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    formatted_text = f"Research Output\nTimestamp: {timestamp}\n\n{data}\n\n"
+    formatted_text = f"--- Research Output ---\nTimestamp: {timestamp}\n\n{data}\n\n"
+
     with open(filename, "a", encoding="utf-8") as f:
         f.write(formatted_text)
+    
     return f"Data successfully saved to {filename}"
 
 def read_from_txt(filename: str = "research_output.txt"):
@@ -20,7 +22,7 @@ def read_from_txt(filename: str = "research_output.txt"):
     
     if not content.strip():
         return "Research file is empty."
-
+    
     return content
 
 save_tool = Tool(
@@ -44,3 +46,5 @@ search_tool = Tool(
 
 api_wrapper = WikipediaAPIWrapper(top_k_results=1, doc_content_chars_max=500)
 wiki_tool = WikipediaQueryRun(api_wrapper=api_wrapper)
+
+arxiv_tool = ArxivQueryRun()
